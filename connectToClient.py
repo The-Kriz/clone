2a935264ab70b15bb1b5b665025fd560983999a6
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2023-05-20 16:38:23 +0530

    Update connectToClient.py

--- connectToClient.py
+++ connectToClient.py
@@ -1,22 +1,33 @@
 import paramiko
-#pip install paramiko
-# SSH credentials
-hostname = 'pics.local'
-username = 'pics'
-password = 'pi'
 
-# SSH into the remote server
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname, username=username, password=password)
-print("hi")
+def ssh_run_command(hostname, username, password, command, input_data=None):
+    # SSH into the remote device
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, username=username, password=password)
 
-# Run the command: python client.py
-command = 'python client.py'
+    # Run the command
+    stdin, stdout, stderr = ssh.exec_command(command)
 
-stdin, stdout, stderr = ssh.exec_command(command)
-print("hi2")
-output = stdout.read().decode('utf-8')
+    # Pass input if provided
+    if input_data:
+        stdin.write(input_data)
+        stdin.flush()
+
+    # Read the output from stdout
+    output = stdout.read().decode('utf-8')
+
+    # Close the SSH connection
+    ssh.close()
+
+    return output
+
+# Example usage
+hostname = 'pics.local'  # Replace with the actual hostname or IP address
+username = 'pics'  # Replace with your SSH username
+password = 'pi'  # Replace with your SSH password
+command = 'python client.py'  # Replace with the command you want to run
+input_data = input("Enter IP address: ")
+
+output = ssh_run_command(hostname, username, password, command, input_data)
 print(output)
-print("hi3")
-
